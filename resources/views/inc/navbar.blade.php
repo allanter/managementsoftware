2f006d3cc303962c2e92d68cf1d4b7eb5f9943e7
Allan Ter <terguoheng@hotmail.com>
2018-07-05 00:15:11 +0800

    edited nav bar
    added logout btn

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,28 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
 
-        <a class="navbar-brand" href="/">Home</a>
+        <!--
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span></button>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+        -->
+        
+        <li class="nav-item">
+        <a class="nav-link" href="/downloads">Downloads</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Logout</a>
+        </li>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        
 
-            <!--
 
-            <li class="nav-item">
-              <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/registration">Registration</a>
-            </li>
-
-            -->
-
-            <li class="nav-item">
-            <a class="nav-link disabled" href="/downloads">Downloads</a>
-            </li>
-            
-          </ul>
-        </div>
-      </nav>
+        
+      </ul>
+    </div>
+  </nav>
